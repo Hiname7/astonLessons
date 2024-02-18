0d7fc479b293ca5efe43a7e1423119e2585365d0
Hiname7 <113289013+Hiname7@users.noreply.github.com>
2024-02-18 09:57:10 +0300

    Delete lesson3 directory

--- lesson3/src/task2.java
+++
@@ -1,9 +0,0 @@
-public class task2 {
-    public static void numberSign(int a){
-        if (a >= 0){
-            System.out.println("Число положительное");
-        } else {
-            System.out.println("Число отрицательное");
-        }
-    }
-}
